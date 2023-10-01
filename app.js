6b56fcaa973f8e83aa0404458e8b45c8573ef8c6
Damilp7 <ayoolabamgbelu@gmail.com>
2023-10-01 13:41:17 +0100

    minor fix

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-let array1 = [15,30,45,60];
-let array2 = [5,6,3,10];
-
-
 let dividedResult = (array1, array2) => {
   if (array1.length === array2.length) {
-    return array1.map((item, index) => item / array2[index]);
+    let result = array1.map((item, index) => item / array2[index]);
+    return result;
   } else{
     return "Number of items in one array does not match the other array";
   }
 }
-
-console.log(dividedResult(array1, array2))
+console.log(dividedResult([15,30,45,60], [5,6,3,10]));
