64c0d9759c2a43b822195d7a6382d34f87513eb9
Damilp7 <ayoolabamgbelu@gmail.com>
2023-10-01 12:49:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let array1 = [15,30,45,60];
+let array2 = [5,6,3,10]
+
+
+let dividedResult = (array1, array2) => {
+  if (array1.length === array2.length) {
+    return array1.map((item, index) => item / array2[index]);
+  } else{
+    return "Number of items in one array does not match the other array";
+  }
+}
+
+console.log(dividedResult(array1, array2))
